6100ee2f806ddbc850ab371ef79b53a28bf649a7
Michael Henretty <michael.henretty@gmail.com>
2014-02-12 18:02:16 -0800

    notifications for demo

--- apps/system/js/notification_toaster.js
+++ apps/system/js/notification_toaster.js
@@ -1,3 +1,5 @@
+/*global NotificationToaster: true */
+
 'use strict';
 
 var NotificationToaster = {
@@ -23,15 +25,17 @@ var NotificationToaster = {
       case 'ftudone':
         this.toaster.addEventListener('tap', this);
         break;
+      case 'tap':
+      case 'mousedown':
+        this.activate();
     }
   },
 
   update: function(detail, type, dir) {
     if (detail.icon) {
       this.toasterIcon.src = detail.icon;
-      this.toasterIcon.hidden = false;
     } else {
-      this.toasterIcon.hidden = true;
+      this.toasterIcon.src = 'style/icons/notification.png';
     }
 
     this.toaster.dataset.notificationId = detail.id;
@@ -45,12 +49,30 @@ var NotificationToaster = {
     this.toasterDetail.dir = dir;
   },
 
+  activate: function() {
+    this.close();
+  },
+
+  close: function() {
+    this.toaster.classList.remove('displayed');
+    this.toaster.classList.remove('fadeDetail');
+    this.toaster.classList.remove('showDetail');
+  },
+
   play: function() {
-    this.toaster.classList.add('displayed');
     this.toaster.addEventListener('animationend', function onDone() {
-      this.toaster.removeEventListener('animationEnd', onDone);
-      this.toaster.classList.remove('displayed');
+      this.toaster.removeEventListener('animationend', onDone);
+      this.toaster.classList.remove('toasty');
+      this.toaster.classList.add('displayed');
+      setTimeout(function() {
+        this.toaster.classList.add('fadeTitle');
+        this.toaster.classList.add('showDetail');
+      }, 1000);
+      setTimeout(function() {
+        this.close();
+      }.bind(this), 3000);
     }.bind(this));
+    this.toaster.classList.add('toasty');
   }
 };
 
