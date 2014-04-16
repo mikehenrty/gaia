a9b5b55c8231161e3d19eca9969e76aa04c6ae1c
Michael Henretty <michael.henretty@gmail.com>
2014-04-15 17:50:51 -0700

    fix touchend events and modularize new objects

--- apps/system/js/notification_toaster.js
+++ apps/system/js/notification_toaster.js
@@ -1,79 +1,82 @@
-/*global NotificationToaster: true */
-
 'use strict';
 
-var NotificationToaster = {
-  toaster: document.getElementById('notification-toaster'),
-  toasterIcon: document.getElementById('toaster-icon'),
-  toasterTitle: document.getElementById('toaster-title'),
-  toasterDetail: document.getElementById('toaster-detail'),
+(function(exports) {
 
-  init: function() {
-    ['tap', 'mousedown', 'swipe'].forEach(function(evt) {
-      this.toaster.addEventListener(evt, this);
-    }, this);
+  var NotificationToaster = {
+    toaster: document.getElementById('notification-toaster'),
+    toasterIcon: document.getElementById('toaster-icon'),
+    toasterTitle: document.getElementById('toaster-title'),
+    toasterDetail: document.getElementById('toaster-detail'),
 
-    window.addEventListener('ftuopen', this);
-    window.addEventListener('ftudone', this);
-  },
+    init: function() {
+      ['tap', 'mousedown', 'swipe'].forEach(function(evt) {
+        this.toaster.addEventListener(evt, this);
+      }, this);
 
-  handleEvent: function(evt) {
-    switch (evt.type) {
-      case 'ftuopen':
-        this.toaster.removeEventListener('tap', this);
-        break;
-      case 'ftudone':
-        this.toaster.addEventListener('tap', this);
-        break;
-      case 'tap':
-      case 'mousedown':
-        this.activate();
+      window.addEventListener('ftuopen', this);
+      window.addEventListener('ftudone', this);
+    },
+
+    handleEvent: function(evt) {
+      switch (evt.type) {
+        case 'ftuopen':
+          this.toaster.removeEventListener('tap', this);
+          break;
+        case 'ftudone':
+          this.toaster.addEventListener('tap', this);
+          break;
+        case 'tap':
+        case 'mousedown':
+          this.activate();
+      }
+    },
+
+    update: function(detail, type, dir) {
+      if (detail.icon) {
+        this.toasterIcon.src = detail.icon;
+      } else {
+        this.toasterIcon.src = 'style/icons/notification.png';
+      }
+
+      this.toaster.dataset.notificationId = detail.id;
+      this.toaster.dataset.type = type;
+      this.toasterTitle.textContent = detail.title;
+      this.toasterTitle.lang = detail.lang;
+      this.toasterTitle.dir = dir;
+
+      this.toasterDetail.textContent = detail.text;
+      this.toasterDetail.lang = detail.lang;
+      this.toasterDetail.dir = dir;
+    },
+
+    activate: function() {
+      this.close();
+    },
+
+    close: function() {
+      this.toaster.classList.remove('displayed');
+      this.toaster.classList.remove('fadeDetail');
+      this.toaster.classList.remove('showDetail');
+    },
+
+    play: function() {
+      this.toaster.addEventListener('animationend', function onDone() {
+        this.toaster.removeEventListener('animationend', onDone);
+        this.toaster.classList.remove('toasty');
+        this.toaster.classList.add('displayed');
+        setTimeout(function() {
+          this.toaster.classList.add('fadeTitle');
+          this.toaster.classList.add('showDetail');
+        }, 1000);
+        setTimeout(function() {
+          this.close();
+        }.bind(this), 3000);
+      }.bind(this));
+      this.toaster.classList.add('toasty');
     }
-  },
+  };
 
-  update: function(detail, type, dir) {
-    if (detail.icon) {
-      this.toasterIcon.src = detail.icon;
-    } else {
-      this.toasterIcon.src = 'style/icons/notification.png';
-    }
+  NotificationToaster.init();
 
-    this.toaster.dataset.notificationId = detail.id;
-    this.toaster.dataset.type = type;
-    this.toasterTitle.textContent = detail.title;
-    this.toasterTitle.lang = detail.lang;
-    this.toasterTitle.dir = dir;
-
-    this.toasterDetail.textContent = detail.text;
-    this.toasterDetail.lang = detail.lang;
-    this.toasterDetail.dir = dir;
-  },
-
-  activate: function() {
-    this.close();
-  },
-
-  close: function() {
-    this.toaster.classList.remove('displayed');
-    this.toaster.classList.remove('fadeDetail');
-    this.toaster.classList.remove('showDetail');
-  },
-
-  play: function() {
-    this.toaster.addEventListener('animationend', function onDone() {
-      this.toaster.removeEventListener('animationend', onDone);
-      this.toaster.classList.remove('toasty');
-      this.toaster.classList.add('displayed');
-      setTimeout(function() {
-        this.toaster.classList.add('fadeTitle');
-        this.toaster.classList.add('showDetail');
-      }, 1000);
-      setTimeout(function() {
-        this.close();
-      }.bind(this), 3000);
-    }.bind(this));
-    this.toaster.classList.add('toasty');
-  }
-};
-
-NotificationToaster.init();
+  exports.NotificationToaster = NotificationToaster;
+}(window));
--- apps/system/js/notification_tray.js
+++ apps/system/js/notification_tray.js
@@ -1,264 +1,257 @@
-/*global NotificationTray: true */
-
 'use strict';
 
-var NotificationTray = {
-  sliding: false,
-  shown: false,
+(function(exports) {
 
-  minimumY: 10, // when sliding, do not display less than 50px
-  yThreshold: 0.25, // open tray after sliding 20%
-  xThreshold: 0.50, // notification dismiss threshold
+  /**
+   * NotificationTray manages notifications in the bottom tray
+   */
+  var NotificationTray = {
+    sliding: false,
+    shown: false,
 
-  screen: document.getElementById('screen'),
-  swipeTarget: document.getElementById('bottom-panel'),
-  container: document.getElementById('notification-tray'),
-  topBar: document.getElementById('notification-bar'),
-  clearAllButton: document.getElementById('notification-clear'),
+    minimumY: 10, // when sliding, do not display less than 50px
+    yThreshold: 0.25, // open tray after sliding 20%
+    xThreshold: 0.50, // notification dismiss threshold
 
-  init: function() {
-    this.swipeTarget.addEventListener('touchstart', this);
-    this.swipeTarget.addEventListener('touchmove', this);
-    this.swipeTarget.addEventListener('touchend', this);
-    this.topBar.addEventListener('touchstart', this);
-    this.topBar.addEventListener('touchmove', this);
-    this.topBar.addEventListener('touchend', this);
+    screen: document.getElementById('screen'),
+    swipeTarget: document.getElementById('bottom-panel'),
+    container: document.getElementById('notification-tray'),
+    topBar: document.getElementById('notification-bar'),
+    clearAllButton: document.getElementById('notification-clear'),
 
-    this.clearAllButton.addEventListener('click',
-      this.clearAll.bind(this), true);
+    init: function() {
+      this.swipeTarget.addEventListener('touchstart', this);
+      this.swipeTarget.addEventListener('touchmove', this);
+      this.swipeTarget.addEventListener('touchend', this);
+      this.topBar.addEventListener('touchstart', this);
+      this.topBar.addEventListener('touchmove', this);
+      this.topBar.addEventListener('touchend', this);
 
-    window.addEventListener('appforeground', this.handleAppopen.bind(this));
-  },
+      this.clearAllButton.addEventListener('click',
+        this.clearAll.bind(this), true);
+    },
 
-  handleEvent: function(evt) {
-    switch (evt.type) {
-      case 'touchstart':
-        this.startSlide(evt.touches[0].pageY);
-        break;
+    handleEvent: function(evt) {
+      switch (evt.type) {
+        case 'touchstart':
+          if (evt.target !== this.clearAllButton) {
+            this.startSlide(evt.touches[0].pageY);
+          }
+          break;
 
-      case 'touchmove':
-        this.slideToY(evt.touches[0].pageY);
-        break;
+        case 'touchmove':
+          this.slideToY(evt.touches[0].pageY);
+          break;
 
-      case 'touchend':
-        this.endSlide(evt.touches[0].pageY);
-        break;
-    }
-  },
-
-  clearAll: function(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-
-    do {
-      var cur = this.container.firstElementChild.nextSibling;
-      NotificationScreen.closeNotification(cur);
-    } while (this.container.firstElementChild.nextSibling);
-  },
-
-  add: function(detail) {
-    var icon;
-    var notificationNode = document.createElement('div');
-    notificationNode.className = 'notification';
-
-    notificationNode.dataset.notificationId = detail.id;
-    notificationNode.dataset.obsoleteAPI = 'false';
-    if (typeof detail.id === 'string' &&
-        detail.id.indexOf('app-notif-') === 0) {
-      notificationNode.dataset.obsoleteAPI = 'true';
-    }
-    var type = notificationNode.dataset.type = detail.type ||
-                                              'desktop-notification';
-    notificationNode.dataset.manifestURL = detail.manifestURL || '';
-
-    if (detail.icon) {
-      icon = document.createElement('img');
-      icon.src = detail.icon;
-      notificationNode.appendChild(icon);
-    }
-
-    var time = document.createElement('span');
-    var timestamp = new Date();
-    time.classList.add('timestamp');
-    time.dataset.timestamp = timestamp;
-    time.textContent = NotificationScreen.prettyDate(timestamp);
-    notificationNode.appendChild(time);
-
-    var dir = (detail.bidi === 'ltr' ||
-               detail.bidi === 'rtl') ?
-          detail.bidi : 'auto';
-    notificationNode.dir = dir;
-
-    var title = document.createElement('div');
-    title.classList.add('title');
-    title.textContent = detail.title;
-    notificationNode.appendChild(title);
-    title.lang = detail.lang;
-    title.dir = dir;
-
-    var message = document.createElement('div');
-    message.classList.add('detail');
-    message.textContent = detail.text;
-    notificationNode.appendChild(message);
-    message.lang = detail.lang;
-    message.dir = dir;
-
-    var notifSelector = '[data-notification-id="' + detail.id + '"]';
-    var oldNotif = this.container.querySelector(notifSelector);
-    if (oldNotif) {
-      // The whole node cannot be replaced because CSS animations are re-started
-      oldNotif.replaceChild(title, oldNotif.querySelector('.title'));
-      oldNotif.replaceChild(message, oldNotif.querySelector('.detail'));
-      oldNotif.replaceChild(time, oldNotif.querySelector('.timestamp'));
-      var oldIcon = oldNotif.querySelector('img');
-      if (icon) {
-        oldIcon ? oldIcon.src = icon.src : oldNotif.insertBefore(icon,
-                                                           oldNotif.firstChild);
-      } else if (oldIcon) {
-        oldNotif.removeChild(oldIcon);
+        case 'touchend':
+          this.endSlide(evt.changedTouches[0].pageY);
+          break;
       }
-      oldNotif.dataset.type = type;
-      notificationNode = oldNotif;
-    } else {
-      notificationNode.addEventListener('touchstart', function(evt) {
-        this.startNotificationSlide(evt.currentTarget, evt.touches[0].pageX);
-      }.bind(this));
-      notificationNode.addEventListener('touchmove', function(evt) {
-        this.slideNotificationToX(evt.currentTarget, evt.touches[0].pageX);
-      }.bind(this));
-      notificationNode.addEventListener('touchend', function(evt) {
-        this.endNotificationSlide(evt.currentTarget, evt.touches[0].pageX);
-      }.bind(this));
-      this.container.insertBefore(notificationNode, this.topBar.nextSibling);
-    }
+    },
 
-    this.clearAllButton.disabled = false;
+    clearAll: function(evt) {
+      evt.stopPropagation();
+      evt.preventDefault();
 
-    return notificationNode;
-  },
+      do {
+        var cur = this.container.firstElementChild.nextSibling;
+        NotificationScreen.closeNotification(cur);
+      } while (this.container.firstElementChild.nextSibling);
+    },
 
-  remove: function(notificationId) {
-    var notifSelector = '[data-notification-id="' + notificationId + '"]';
-    var notificationNode = this.container.querySelector(notifSelector);
-    if (notificationNode) {
-      notificationNode.parentNode.removeChild(notificationNode);
-    }
-    // if this is the only notification left
-    if (!this.container.firstElementChild) {
-      // no notifications left
-      this.clearAllButton.disabled = true;
-    }
-  },
+    add: function(detail) {
+      var icon;
+      var notificationNode = document.createElement('div');
+      notificationNode.className = 'notification';
 
-  startSlide: function(y) {
-    this.screenHeight = this.screen.getBoundingClientRect().height;
-    this.containerHeight = this.container.getBoundingClientRect().height;
-    this.sliding = true;
-    this.startY = y;
-    this.slideToY(y);
-  },
+      notificationNode.dataset.notificationId = detail.id;
+      notificationNode.dataset.obsoleteAPI = 'false';
+      if (typeof detail.id === 'string' &&
+          detail.id.indexOf('app-notif-') === 0) {
+        notificationNode.dataset.obsoleteAPI = 'true';
+      }
+      var type = notificationNode.dataset.type = detail.type ||
+                                                'desktop-notification';
+      notificationNode.dataset.manifestURL = detail.manifestURL || '';
 
-  slideToY: function(y) {
-    if (!this.sliding) {
-      return;
-    }
-    if (y < 0) {
-      y = 0;
-    }
-    var yFromBottom = this.screenHeight - y;
-    if (yFromBottom < this.minimumY) {
+      if (detail.icon) {
+        icon = document.createElement('img');
+        icon.src = detail.icon;
+        notificationNode.appendChild(icon);
+      }
+
+      var time = document.createElement('span');
+      var timestamp = new Date();
+      time.classList.add('timestamp');
+      time.dataset.timestamp = timestamp;
+      time.textContent = NotificationScreen.prettyDate(timestamp);
+      notificationNode.appendChild(time);
+
+      var dir = (detail.bidi === 'ltr' ||
+                 detail.bidi === 'rtl') ?
+            detail.bidi : 'auto';
+      notificationNode.dir = dir;
+
+      var title = document.createElement('div');
+      title.classList.add('title');
+      title.textContent = detail.title;
+      notificationNode.appendChild(title);
+      title.lang = detail.lang;
+      title.dir = dir;
+
+      var message = document.createElement('div');
+      message.classList.add('detail');
+      message.textContent = detail.text;
+      notificationNode.appendChild(message);
+      message.lang = detail.lang;
+      message.dir = dir;
+
+      var notifSelector = '[data-notification-id="' + detail.id + '"]';
+      var oldNotif = this.container.querySelector(notifSelector);
+      if (oldNotif) {
+        // The whole node cannot be replaced cuz CSS animations are re-started
+        oldNotif.replaceChild(title, oldNotif.querySelector('.title'));
+        oldNotif.replaceChild(message, oldNotif.querySelector('.detail'));
+        oldNotif.replaceChild(time, oldNotif.querySelector('.timestamp'));
+        var oldIcon = oldNotif.querySelector('img');
+        if (icon) {
+          oldIcon ? oldIcon.src = icon.src : oldNotif.insertBefore(icon,
+                                                      oldNotif.firstChild);
+        } else if (oldIcon) {
+          oldNotif.removeChild(oldIcon);
+        }
+        oldNotif.dataset.type = type;
+        notificationNode = oldNotif;
+      } else {
+        notificationNode.addEventListener('touchstart', function(evt) {
+          this.startNotificationSlide(evt.currentTarget, evt.touches[0].pageX);
+        }.bind(this));
+        notificationNode.addEventListener('touchmove', function(evt) {
+          this.slideNotificationToX(evt.currentTarget, evt.touches[0].pageX);
+        }.bind(this));
+        notificationNode.addEventListener('touchend', function(evt) {
+          this.endNotificationSlide(evt.currentTarget,
+                                    evt.changedTouches[0].pageX);
+        }.bind(this));
+        this.container.insertBefore(notificationNode, this.topBar.nextSibling);
+      }
+
+      this.clearAllButton.disabled = false;
+
+      return notificationNode;
+    },
+
+    remove: function(notificationId) {
+      var notifSelector = '[data-notification-id="' + notificationId + '"]';
+      var notificationNode = this.container.querySelector(notifSelector);
+      if (notificationNode) {
+        notificationNode.parentNode.removeChild(notificationNode);
+      }
+      // if this is the only notification left
+      if (!this.container.firstElementChild) {
+        // no notifications left
+        this.clearAllButton.disabled = true;
+      }
+    },
+
+    startSlide: function(y) {
+      this.screenHeight = this.screen.getBoundingClientRect().height;
+      this.containerHeight = this.container.getBoundingClientRect().height;
+      this.sliding = true;
+      this.startY = y;
+      this.slideToY(y);
+    },
+
+    slideToY: function(y) {
+      if (!this.sliding) {
+        return;
+      }
+      if (y < 0) {
+        y = 0;
+      }
+      var yFromBottom = this.screenHeight - y;
+      if (yFromBottom < this.minimumY) {
+        this.container.classList.remove('no-transition');
+        this.container.style.transform = 'translateY(-' + this.minimumY + 'px)';
+      } else {
+        this.container.classList.add('no-transition');
+        this.container.style.transform = 'translateY(-' + yFromBottom + 'px)';
+      }
+    },
+
+    endSlide: function(y) {
+      this.sliding = false;
+      var pixelsFromStart;
+      // decide if we have slid enough pixels
+      // to toggle the notification tray
+      if (this.shown) {
+        pixelsFromStart = y;
+      } else {
+        pixelsFromStart = this.screenHeight - y;
+      }
+      if (pixelsFromStart / this.screenHeight > this.yThreshold) {
+        this.toggle();
+      } else {
+        this.cancelSlide();
+      }
+    },
+
+    cancelSlide: function() {
+      if (this.shown) {
+        this.show();
+      } else {
+        this.hide();
+      }
+    },
+
+    toggle: function() {
+      if (this.shown) {
+        this.hide();
+      } else {
+        this.show();
+      }
+    },
+
+    show: function() {
+      this.shown = true;
       this.container.classList.remove('no-transition');
-      this.container.style.transform = 'translateY(-' + this.minimumY + 'px)';
-    } else {
-      this.container.classList.add('no-transition');
-      this.container.style.transform = 'translateY(-' + yFromBottom + 'px)';
-    }
-  },
+      this.container.style.transform = 'translateY(-100%)';
+    },
 
-  endSlide: function(y) {
-    this.sliding = false;
-    var pixelsFromStart;
-    // decide if we have slid enough pixels
-    // to toggle the notification tray
-    if (this.shown) {
-      pixelsFromStart = y;
-    } else {
-      pixelsFromStart = this.screenHeight - y;
-    }
-    if (pixelsFromStart / this.screenHeight > this.yThreshold) {
-      this.toggle();
-    } else {
-      this.cancelSlide();
-    }
-  },
+    hide: function() {
+      this.shown = false;
+      this.container.classList.remove('no-transition');
+      this.container.style.transform = 'translateY(0)';
+    },
 
-  cancelSlide: function() {
-    if (this.shown) {
-      this.show();
-    } else {
-      this.hide();
-    }
-  },
+    startNotificationSlide: function(el, x) {
+      el._startX = x;
+      el.classList.add('no-transition');
+    },
 
-  toggle: function() {
-    if (this.shown) {
-      this.hide();
-    } else {
-      this.show();
-    }
-  },
-
-  show: function() {
-    this.shown = true;
-    this.container.classList.remove('no-transition');
-    this.container.style.transform = 'translateY(-100%)';
-  },
-
-  hide: function() {
-    this.shown = false;
-    this.container.classList.remove('no-transition');
-    this.container.style.transform = 'translateY(0)';
-  },
-
-  startNotificationSlide: function(el, x) {
-    el._startX = x;
-    el.classList.add('no-transition');
-  },
-
-  slideNotificationToX: function(el, x) {
-    var deltaX = x - el._startX;
-    if (deltaX > 0) {
-      el.style.transform = 'translateX(' + deltaX + 'px)';
-    }
-  },
-
-  endNotificationSlide: function(el, x) {
-    el.classList.remove('no-transition');
-    var deltaX = x - el._startX;
-    if (deltaX / el.getBoundingClientRect().width > this.xThreshold) {
-      el.style.transform = 'translateX(100%)';
-      el.addEventListener('transitionend', function onEnd() {
-        el.removeEventListener('transitionend', onEnd);
-        NotificationScreen.closeNotification(el);
-      });
-    } else {
-      el.style.transform = 'translateX(0)';
-    }
-  },
-
-  // TODO: Remove this when we ditch mozNotification (bug 952453)
-  handleAppopen: function ns_handleAppopen(evt) {
-    var manifestURL = evt.detail.manifestURL,
-        selector = '[data-manifest-u-r-l="' + manifestURL + '"]';
-
-    var nodes = this.container.querySelectorAll(selector);
-
-    for (var i = nodes.length - 1; i >= 0; i--) {
-      if (nodes[i].dataset.obsoleteAPI === 'true') {
-        NotificationScreen.removeNotification(nodes[i].dataset.notificationId);
+    slideNotificationToX: function(el, x) {
+      var deltaX = x - el._startX;
+      if (deltaX > 0) {
+        el.style.transform = 'translateX(' + deltaX + 'px)';
       }
-    }
-  }
-};
+    },
 
-NotificationTray.init();
+    endNotificationSlide: function(el, x) {
+      el.classList.remove('no-transition');
+      var deltaX = x - el._startX;
+      if (deltaX / el.getBoundingClientRect().width > this.xThreshold) {
+        el.style.transform = 'translateX(100%)';
+        el.addEventListener('transitionend', function onEnd() {
+          el.removeEventListener('transitionend', onEnd);
+          NotificationScreen.closeNotification(el);
+        });
+      } else {
+        el.style.transform = 'translateX(0)';
+      }
+    },
+  };
+
+  NotificationTray.init();
+
+  exports.NotificationTray = NotificationTray;
+}(window));
